37561f6a3d2d2b7729a442e5e979e7829b2ff1b1
AVS1508 <aditya.11.present@gmail.com>
2023-01-18 13:54:58 +0530

    Performed two-fold separation of concerns: theming vs. color and constants vs. utilities

---
+++ lib/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const trueWhite = Color.fromRGBO(255, 255, 255, 1);
+  static const trueBlack = Color.fromRGBO(0, 0, 0, 1);
+  static const offWhite = Color.fromRGBO(241, 240, 234, 1);
+  static const charadeBlack = Color.fromRGBO(39, 40, 56, 1);
+  static const foliageGreen = Color.fromRGBO(100, 183, 70, 1);
+  static const errorRed = Color.fromRGBO(209, 0, 0, 1);
+  static const materialBlue = Color.fromRGBO(68, 138, 255, 1);
+  static const cadetGrey = Color.fromRGBO(93, 115, 126, 1);
+}
