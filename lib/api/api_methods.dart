cb747ef04b6a7ea47d663bb64f037df72316a77a
AVS1508 <aditya.11.present@gmail.com>
2023-01-25 10:37:23 +0530

    Updated API method to use less-bandwidth API endpoint

--- lib/api/api_methods.dart
+++ lib/api/api_methods.dart
@@ -6,10 +6,11 @@ import 'package:http/http.dart' as http;
 
 Future<double> getCoinPrice(String id) async {
   try {
-    var url = Uri.parse('https://api.coingecko.com/api/v3/coins/$id');
+    var url = Uri.parse(
+        'https://api.coingecko.com/api/v3/simple/price?ids=$id&vs_currencies=usd&precision=4');
     var response = await http.get(url);
     var data = jsonDecode(response.body);
-    return double.parse(data['market_data']['current_price']['usd'].toString());
+    return double.parse(data[id]['usd'].toString());
   } catch (e) {
     print(e.toString());
     return 0.0;
